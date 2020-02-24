fab47f91df5b18887781794780738c3a030b206d
fredbenevides <fhfreitas@gmail.com>
2020-02-23 21:34:35 -0500

    REST tested

---
+++ src/main/java/com/fredbenevides/cursomc/resources/CategoryResource.java
@@ -0,0 +1,15 @@
+package com.fredbenevides.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listing() {
+		return "Rest is working";
+	}
+}
