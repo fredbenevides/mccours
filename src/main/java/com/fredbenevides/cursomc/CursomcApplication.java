79877f2aa747b12fb4e6f1e317025c2b4e2cf67e
fredbenevides <fhfreitas@gmail.com>
2020-02-23 22:20:01 -0500

    Created instatiation operation

--- src/main/java/com/fredbenevides/cursomc/CursomcApplication.java
+++ src/main/java/com/fredbenevides/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.fredbenevides.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fredbenevides.cursomc.domain.Category;
+import com.fredbenevides.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Info");
+		Category cat2 = new Category(null, "Escritorio");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
