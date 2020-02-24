62fe5944a815d69f78a7e1c398a71152b5d628fc
fredbenevides <fhfreitas@gmail.com>
2020-02-23 22:48:28 -0500

    Adjusted endpoint /categories/{id}

--- src/main/java/com/fredbenevides/cursomc/services/CategoryService.java
+++ src/main/java/com/fredbenevides/cursomc/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fredbenevides.cursomc.domain.Category;
 import com.fredbenevides.cursomc.repositories.CategoryRepository;
+import com.fredbenevides.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,7 +17,7 @@ public class CategoryService {
 	
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
 	}
-
 }
