4a40ee08582903489622fd6eca78a09fac6e2a2d
fredbenevides <fhfreitas@gmail.com>
2020-02-23 21:42:52 -0500

    Creating and testing first domain class

--- src/main/java/com/fredbenevides/cursomc/resources/CategoryResource.java
+++ src/main/java/com/fredbenevides/cursomc/resources/CategoryResource.java
@@ -1,15 +1,29 @@
 package com.fredbenevides.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fredbenevides.cursomc.domain.Category;
+
 @RestController
-@RequestMapping(value="/categories")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listing() {
-		return "Rest is working";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Category> listing() {
+
+		Category cat1 = new Category(1, "Informatique");
+		Category cat2 = new Category(2, "Bureau");
+
+		List<Category> list = new ArrayList<>();
+
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
+
 }
