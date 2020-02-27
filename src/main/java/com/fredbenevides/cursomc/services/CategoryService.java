531a8daba3d9bc1298ca5edd1bb2b4a96685f4d0
fredbenevides <fhfreitas@gmail.com>
2020-02-26 19:03:46 -0500

    Created Delete for Category and managed DataIntegrity exception

--- src/main/java/com/fredbenevides/cursomc/services/CategoryService.java
+++ src/main/java/com/fredbenevides/cursomc/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.fredbenevides.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fredbenevides.cursomc.domain.Category;
 import com.fredbenevides.cursomc.repositories.CategoryRepository;
+import com.fredbenevides.cursomc.services.exceptions.DataIntegrityException;
 import com.fredbenevides.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,17 @@ public class CategoryService {
 	public Category update(Category obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("It is impossible to delete a category which has products!");
+		}
+		
+		
 	}
 }
